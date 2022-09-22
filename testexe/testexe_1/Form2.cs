c03f84703b5445daa4118b823cd2ce7a4d7a0a7a
Title <Title@TITLE-PC>
2022-09-22 17:39:50 +0700

    ehgfdfdd

---
+++ testexe/testexe_1/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testexe_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double h, w, a;
+            h = double.Parse(this.textBox1.Text);
+            w = double.Parse(this.textBox2.Text);
+            a = h * w;
+            textBox3.Text = a.ToString();
+
+        }
+    }
+}
---
+++ testexe/testexe_1/Form3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testexe_1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double h, w, a;
+            const double pi = 3.14;
+            h = double.Parse(this.textBox1.Text);
+
+            a = pi*Math.Pow(h,2);
+            textBox3.Text = a.ToString();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode== Keys.Enter)
+            {
+                double h, w, a;
+                const double pi = 3.14;
+                h = double.Parse(this.textBox1.Text);
+
+                a = pi * Math.Pow(h, 2);
+                textBox3.Text = a.ToString();
+            }
+        }
+    }
+}
